0d246b03b2341356422f8db0f3c0de40e42d1823
Nicolas Choquet <nicolachoquet06250@gmail.com>
2022-01-08 19:55:46 +0100

    ajout d'un fond blanc pour l'application about

--- src/views/About.vue
+++ src/views/About.vue
@@ -8,5 +8,16 @@
 
 <script setup>
 import MacApplication from '@/components/MacApplication.vue';
+
+const dockHeight = document.querySelector('.dock__wrapper').offsetHeight;
 </script>
+
+<style lang="scss" scoped>
+.app-about {
+  background: white;
+  height: 100%;
+
+  h1 { margin: 0; }
+}
+</style>
 
