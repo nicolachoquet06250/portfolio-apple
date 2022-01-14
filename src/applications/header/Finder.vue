4d79204ce24e823e28c916270bcf81189400e410
Choquet Nicolas <nicolachoquet06250@gmail.com>
2022-01-14 14:02:47 +0100

    Add files via upload

---
+++ src/applications/header/Finder.vue
@@ -0,0 +1,144 @@
+<template>
+<div class="left-side">
+    <button>
+        <i class="fas fa-chevron-left"></i>
+    </button>
+
+    <button>
+        <i class="fas fa-chevron-right"></i>
+    </button>
+
+    <h1> {{ selectedTab }} </h1>
+</div>
+
+<div class="right-side">
+    <div>
+        <button>
+            <i class="fas fa-border-all"></i>
+            <i class="fas fa-angle-down"></i>
+        </button>
+
+        <button>
+            <i class="fas fa-table"></i>
+            <i class="fas fa-angle-down"></i>
+        </button>
+
+        <button>
+            <i class="fas fa-ellipsis-h"></i>
+            <i class="fas fa-angle-down"></i>
+        </button>
+    </div>
+
+    <div>
+        <button>
+            <i class="fas fa-upload"></i>
+        </button>
+        
+        <button>
+            <i class="fas fa-tag"></i>
+        </button>
+        
+        <button>
+            <i class="fas fa-angle-double-right"></i>
+        </button>
+        
+        <button>
+            <i class="fas fa-search" style="font-size: 20px; margin-right: 5px; color: #C5C5C5;"></i>
+        </button>
+    </div>
+</div>
+</template>
+
+<script setup>
+import { computed } from 'vue';
+import { useCurrentApp } from '@/hooks/apps';
+
+const { currentAppHeaderBar } = useCurrentApp();
+
+const selectedTab = computed(() => currentAppHeaderBar.value?.left?.[2]?.text ?? '');
+</script>
+
+<style lang="scss">
+.app-header-bar {
+    .left-side {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        flex: 1;
+
+        button {
+            width: 40px;
+            height: 40px;
+            font-size: 20px;
+            margin-right: 10px;
+            color: #C5C5C5;
+            background-color: transparent;
+            border: none;
+
+            i.fas {
+                color: #C5C5C5;
+            }
+        }
+
+        h1 {
+            margin: 0;
+            margin-right: 10px;
+        }
+    }
+
+    .right-side {
+        display: flex;
+        flex-direction: row;
+        flex: 1;
+        justify-content: space-between;
+        align-items: center;
+
+        > div {
+            display: flex;
+            flex-direction: row;
+            align-items: center;
+
+            button {
+                width: 40px;
+                height: 40px;
+                font-size: 25px;
+                margin-right: 15px;
+                color: #C5C5C5;
+                background-color: transparent;
+                border: none;
+                display: flex;
+                flex-direction: row;
+                align-items: center;
+                justify-content: center;
+
+                i.fas {
+                    &:first-child {
+                        font-size: 20px; 
+                        margin-right: 5px; 
+                        color: #C5C5C5;
+                    }
+
+                    &:last-child {
+                        font-size: 15px; 
+                        color: #C5C5C5;
+                    }
+                }
+            }
+
+            &:last-child {
+                button {
+                    margin-right: unset;
+                    
+                    i.fas {
+                        font-size: unset; 
+                    }
+
+                    &:last-child {
+                        font-size: 20px;
+                    }
+                }
+            }
+        }
+    }
+}
+</style>
