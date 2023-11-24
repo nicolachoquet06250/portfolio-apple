a1c648452d4e6572a3d3a2d93fd38876106bf45d
Nicolas Choquet <nchoquet@norsys.fr>
2023-11-24 15:42:32 +0100

    - Update de l'app avec vite.js - Fix du bug en mode sans install au demarage du webos

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,1 +1,1 @@
-ssh nicolas-choquet@ssh-nicolas-choquet.alwaysdata.net 'cd portfolio-apple && npm run update-prod'
+ssh nicolas-choquet@ssh-nicolas-choquet.alwaysdata.net 'cd portfolio-apple && git pull && ./bin/yarn && ./bin/yarn update-prod'
