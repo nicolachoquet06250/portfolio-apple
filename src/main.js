d295853f838ea426b70447943b41fd194c05b3ba
Choquet Nicolas <nicolachoquet06250@gmail.com>
2023-11-30 20:35:38 +0000

    Ajout d'une console en mode dev pour les navigateurs mobiles

--- src/main.js
+++ src/main.js
@@ -2,4 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
 
+if (import.meta.env.MODE === 'development') {
+    import('https://cdn.jsdelivr.net/npm/eruda').then(() => { 
+        eruda.init()
+    });
+}
+
 createApp(App).mount('#app')
