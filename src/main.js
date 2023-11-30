fea452a7d9d26b52937ec670b09d3dd2bdcb1a25
Choquet Nicolas <nicolachoquet06250@gmail.com>
2023-11-30 21:58:35 +0000

    Ajout de certains plugins pour Eruda en dev

--- src/main.js
+++ src/main.js
@@ -3,9 +3,20 @@ import App from './App.vue'
 import './registerServiceWorker'
 
 if (import.meta.env.MODE === 'development') {
-    import('https://cdn.jsdelivr.net/npm/eruda').then(() => { 
+    (async () => {
+        await import('https://cdn.jsdelivr.net/npm/eruda');
+        await import('https://cdn.jsdelivr.net/npm/eruda-dom@2.0.0/eruda-dom.min.js');
+        await import('https://cdn.jsdelivr.net/npm/eruda-features@2.0.0/eruda-features.min.js');
+        await import('https://cdn.jsdelivr.net/npm/eruda-timing@2.0.0/eruda-timing.min.js');
+        await import('https://cdn.jsdelivr.net/npm/eruda-code@2.0.0/eruda-code.min.js');
+         
         eruda.init()
-    });
+
+        eruda.add(erudaDom)
+        eruda.add(erudaFeatures)
+        eruda.add(erudaTiming)
+        eruda.add(erudaCode)
+    })()
 }
 
 createApp(App).mount('#app')
