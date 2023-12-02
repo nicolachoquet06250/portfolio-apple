88709152d2e59da1ee0e4f068b369dc6d180c1e4
Nicolas Choquet <nchoquet@norsys.fr>
2023-12-02 15:23:19 +0100

    Amélioration du score PWA de l'app

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,1 +1,7 @@
-ssh nicolas-choquet@ssh-nicolas-choquet.alwaysdata.net 'cd portfolio-apple && rm -f yarn.lock && rm -f package.lock && git pull && npm install && npm run update-prod'
+ssh nicolas-choquet@ssh-nicolas-choquet.alwaysdata.net \
+  'cd portfolio-apple && \
+    rm -f yarn.lock && \
+    rm -f package.lock && \
+    git pull && \
+    npm install && \
+    npm run build'
