8c23dbf67f24e4feb425598f10af58d5a0e90326
Nicolas Choquet <nchoquet@norsys.fr>
2023-12-05 11:46:37 +0100

    Test PWA

--- public/sw.js
+++ public/sw.js
@@ -1,31 +1,50 @@
 // This is the "Offline page" service worker
 
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+// importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+//
+// const CACHE = "pwabuilder-page";
+// const offlineFallbackPage = "index.html";
+//
+// self.addEventListener("message", (event) => {
+//     if (event.data && event.data.type === "SKIP_WAITING") {
+//         self.skipWaiting();
+//     }
+// });
+//
+// self.addEventListener('install', async (event) => {
+//     event.waitUntil(
+//         caches.open(CACHE)
+//             .then((cache) => cache.add(offlineFallbackPage))
+//     );
+// });
+//
+// if (workbox.navigationPreload.isSupported()) {
+//     workbox.navigationPreload.enable();
+// }
 
-const CACHE = "pwabuilder-page";
+const cacheName = 'v1';
+const offlineFallbackPage = 'index.html';
 
-const offlineFallbackPage = "index.html";
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(cacheName)
+            .then(cache => cache.addAll([
+                offlineFallbackPage,
+                'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css',
+                '/img/manifest-icon-512.maskable.png'
+            ]))
+    );
+});
 
-self.addEventListener("message", (event) => {
-    if (event.data && event.data.type === "SKIP_WAITING") {
+self.addEventListener('message', function (event) {
+    if (event.data.action === 'skipWaiting') {
         self.skipWaiting();
     }
 });
 
-self.addEventListener('install', async (event) => {
-    event.waitUntil(
-        caches.open(CACHE)
-            .then((cache) => cache.add(offlineFallbackPage))
-    );
-});
-
-if (workbox.navigationPreload.isSupported()) {
-    workbox.navigationPreload.enable();
-}
-
 self.addEventListener('fetch', (event) => {
     if (event.request.mode === 'navigate') {
-        event.respondWith((async () => {
+        event.respondWith(/*(async () => {
             try {
                 return ((preloadResp = await event.preloadResponse) && preloadResp)
                     ? preloadResp
@@ -34,6 +53,14 @@ self.addEventListener('fetch', (event) => {
                 const cache = await caches.open(CACHE);
                 return await cache.match(offlineFallbackPage);
             }
-        })());
+        })()*/
+            caches.match(event.request)
+                .then(function (response) {
+                    if (response) {
+                        return response;
+                    }
+                    return fetch(event.request);
+                })
+        );
     }
 });
--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -16,8 +16,25 @@ if (process.env.NODE_ENV === 'production') {
     cached () {
       console.log('Content has been cached for offline use.')
     },
-    updatefound () {
+    updatefound (reg) {
       console.log('New content is downloading.')
+
+      // A wild service worker has appeared in reg.installing!
+      const newWorker = reg.installing;
+
+      newWorker.addEventListener('statechange', () => {
+        // Has network.state changed?
+        switch (newWorker.state) {
+          case 'installed':
+            if (navigator.serviceWorker.controller) {
+              // new update available
+              // showUpdateBar();
+              console.log('show update')
+            }
+            // No update available
+            break;
+        }
+      });
     },
     updated () {
       console.log('New content is available; please refresh.')
