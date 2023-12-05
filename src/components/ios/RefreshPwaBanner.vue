882ed5b40b511f1029fc85c1e6511084caf3c60e
Nicolas Choquet <nchoquet@norsys.fr>
2023-12-05 11:56:15 +0100

    Test PWA

---
+++ src/components/ios/RefreshPwaBanner.vue
@@ -0,0 +1,34 @@
+<template>
+  <Notification
+      v-if="showRefreshPageNotification"
+      :icon="installIcon"
+      :date="formattedDate"
+  >
+      <template #title>
+        Mise à jour système
+      </template>
+
+      <p> Une mise à jour du portefolio est disponible ! </p>
+      <p> Veuillez rafraichir </p>
+
+      <template #actions>
+          <button @click="window.location.reload()">
+              Rafraichir
+          </button>
+      </template>
+  </Notification>
+</template>
+
+<script setup>
+import Notification from "@/components/ios/UnlockScreenNotification.vue";
+import installIcon from "@/assets/icon-cd-install-mac.png";
+import {showRefreshPageNotification} from '@/hooks/pwa';
+
+const date = new Date();
+const formattedDate = `${date.getHours() < 10 ? '0' : ''}${date.getHours()}:${date.getMinutes() < 10 ? '0' : ''}${date.getMinutes()}`
+
+</script>
+
+<style scoped lang="scss">
+
+</style>
