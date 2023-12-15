6e0fda1b6038c08604c399cbdb3134fd7a3b291a
Nicolas Choquet <nchoquet@norsys.fr>
2023-12-15 09:56:36 +0100

    Construction du terminal: - Gestion de la commande 'cd':  - Gestion des chemins absoluts et relatifs grace à une fonction realpath ( inspirée de la fonction de php )  - Gestion des retours arrière via '..'  - Gestion des espaces dans les noms de répertoire

--- src/commands/cd.ts
+++ src/commands/cd.ts
@@ -1,8 +1,8 @@
 import type {Setter, TerminalCommandExecute} from '@/commands/types';
 import finder from '@/hooks/finder';
 
-export const command = /^cd (?<dist>[a-zA-Z0-9_\-\/]+)$/g;
-export const adminCommand = /^sudo cd (?<dist>[a-zA-Z0-9_\-\/]+)$/g;
+export const command = /^cd (?<dist>([a-zA-Z0-9_\-\/.]+|'[a-zA-Z0-9_\-\/. ]+'))$/g;
+export const adminCommand = /^sudo cd (?<dist>([a-zA-Z0-9_\-\/.]+|'[a-zA-Z0-9_\-\/. ]+'))$/g;
 
 type Props = {
     dist: string
@@ -14,29 +14,70 @@ type Setters = {
 
 const { getChildrenItems, isPathExists } = finder();
 
+type RealPath = (path: string) => string;
+
+const realpath: RealPath = path => {
+    const p = path.split('/');
+    if ([...p].pop() === '') p.pop();
+    if ([...p].shift() === '') p.shift();
+
+    if (path.includes('..')) {
+
+        const newP: string[] = [];
+        for (const it of p) {
+            if (it === '..') {
+                if (p.length > 0) {
+                    newP.pop();
+                }
+            }
+            else {
+                newP.push(it)
+            }
+        }
+
+        return '/' + newP.join('/');
+    }
+
+    return '/' + p.join('/');
+};
+
 export const execute: TerminalCommandExecute<Props, Setters> = (
     {dist},
     _isAdmin,
     location,
     {location: setLocation}
 ) => {
-    // @TODO implementer les retours arrière '../'
-    // @TODO Fixer la partie chemins absoluts
-    /*if (dist.startsWith('/')) {
-        if (isPathExists(dist.replace('//', '/'))) {
-            setLocation(dist.replace('//', '/'));
+    if (dist.startsWith('\'')) dist = dist.substring(1);
+    if (dist.endsWith('\'')) dist = dist.substring(0, dist.length - 1);
+
+    if (dist.startsWith('/')) {
+        if (isPathExists(realpath(dist.replace('//', '/')))) {
+            setLocation(realpath(dist.replace('//', '/')));
             console.log(getChildrenItems()('', location.value))
             return [``];
         }
 
         return `cd: ${dist}: Aucun fichier ou dossier de ce type`
-    } else {*/
-        if (isPathExists((location.value + '/' + dist).replace('//', '/'))) {
-            setLocation(l => (l + '/' + dist).replace('//', '/'));
+    }
+    else {
+        if (
+            isPathExists(
+                realpath(
+                    (location.value + '/' + dist)
+                        .replace('//', '/')
+                )
+            )
+        ) {
+            setLocation(l =>
+                realpath(
+                    (l + '/' + dist)
+                        .replace('//', '/')
+                )
+            );
             console.log(getChildrenItems()('', location.value))
             return [``];
         }
 
         return `cd: ${dist}: Aucun fichier ou dossier de ce type`
-    /*}*/
+    }
 }
